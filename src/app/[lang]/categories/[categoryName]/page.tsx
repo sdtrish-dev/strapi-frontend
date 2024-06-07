4597bdeb7efed72620b53fc76c5e352c1da01050
Trish Gill <95247759+sdtrish-dev@users.noreply.github.com>
2024-06-07 17:00:48 -0400

    [chore] add to do list

--- src/app/[lang]/categories/[categoryName]/page.tsx
+++ src/app/[lang]/categories/[categoryName]/page.tsx
@@ -1,35 +1,16 @@
 'use client';
-import { useRouter } from 'next/router';
+
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Loader from './../../components/Loader';
 
 export default function CategoryPage() {
-  const router = useRouter();
-  const { categoryName } = router.query;
-  const [categoryData, setCategoryData] = useState(null);
-  const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (categoryName) {
-      const fetchCategoryData = async () => {
-        try {
-          const response = await axios.get(`/api/categories?slug=${categoryName}`);
-          setCategoryData(response.data);
-        } catch (error) {
-          console.error('Error fetching category data:', error);
-        } finally {
-          setLoading(false);
-        }
-      };
+// TODO
 
-      fetchCategoryData();
-    }
-  }, [categoryName]);
+//   if (isLoading) return <Loader />;
 
-  if (isLoading) return <Loader />;
-
-  if (!categoryData) return <div>Category not found</div>;
+//   if (!categoryData) return <div>Category not found</div>;
 
   return (
     <div>
